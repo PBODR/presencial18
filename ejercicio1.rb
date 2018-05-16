67c2030878466d271b7798ee7c4ddd514c060c1e
pbodr <pbolivares@gmail.com>
2018-05-16 19:25:03 -0400

    base ejercicios

---
+++ ejercicio1.rb
@@ -0,0 +1,20 @@
+Ejercicio 1: Objetos y archivos.
+El archivo casino.txt contiene las recaudaciones de 4 días de un casino separadas por mesa de juego.
+
+Mesa 1, 90, 60, 10, 30
+Mesa 2, 40, 34, 77, 11
+Mesa 3, 34, 86, 55, 91
+Mesa 4, 67, 93, 43, 87
+Se pide:
+
+Crear una clase Table cuyo constructor reciba el nombre de la mesa y las recaudaciones correspondientes de cada día.
+
+Hint: El constructor debe recibir 5 argumentos. Uno para el nombre de la mesa y los demás para cada recaudación correspondiente a esa mesa.
+
+Crear un método que permita leer el archivo e instanciar un objeto de la clase Table por cada línea del archivo.
+
+Crear métodos que permitan:
+
+Conocer el mayor valor recaudado, por mesa, y el nombre de la mesa y día corresponde (día 1, día 2, día 3 o día 4).
+
+Calcular el promedio total de lo recaudado por todas las mesas en todos los días.
